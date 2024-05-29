bad1cbc628ca75fd88e4fde534613e7168dbcf28
David Pereira <davidmarlonpereira@gmail.com>
2024-05-29 20:06:45 -0300

    - Criar Conexão Adicionado dependencias do yml e postgres no maven Criado Classe ConnectionFactory para conexão dod banco Criado yml de conexão do postgres

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module trabalho_banco_2.trabalhobd2 {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.sql;
 
     opens trabalho_banco_2.trabalhobd2 to javafx.fxml;
     exports trabalho_banco_2.trabalhobd2;
