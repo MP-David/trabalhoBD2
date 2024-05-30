88be1787a4045148473b5b12ccc349ef131c448b
David Pereira <davidmarlonpereira@gmail.com>
2024-05-30 15:53:24 -0300

    adequando projeto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module trabalho_banco_2.trabalhobd2 {
-    requires javafx.controls;
-    requires javafx.fxml;
+    module trabalho_banco_2.trabalhobd2 {
+        requires javafx.controls;
+        requires javafx.fxml;
 
-    requires com.dlsc.formsfx;
-    requires java.sql;
+        requires com.dlsc.formsfx;
+        requires java.sql;
 
-    opens trabalho_banco_2.trabalhobd2 to javafx.fxml;
-    exports trabalho_banco_2.trabalhobd2;
-}
+        opens trabalho_banco_2.trabalhobd2 to javafx.fxml;
+        exports trabalho_banco_2.trabalhobd2;
+    }
